f3a3e8e2cd53f0150550058b7849065f5d5c2441
Afzal Lakdawala <afzalmlakdawala@gmail.com>
2025-12-25 16:16:47 +0530

    Fix static files storage for development - use simple storage when DEBUG=True

--- gitlab_mcp_bridge/settings.py
+++ gitlab_mcp_bridge/settings.py
@@ -139,7 +139,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise configuration for serving static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Use manifest storage only in production (requires collectstatic)
+# In development, use simple storage that doesn't require manifest
+if DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+else:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
